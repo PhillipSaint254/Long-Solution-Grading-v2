b196dbf567f91750817909a83b92dd24ce277a04
Phillip Santei <phillipsantei254@gmail.com>
2025-03-31 18:01:34 +0300

    Worked on 'Databases - Fetching and Displaying the Top 5 Databases of 2023 from DB-Engines', finished the last three

--- Production/Data Analysis - Comparison of Ophthalmologist Density Across European Countries/_ten.py
+++ Production/Data Analysis - Comparison of Ophthalmologist Density Across European Countries/_ten.py
@@ -1,6 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
+def get_countries():
+    return [
+        'Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium',
+        'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 
+        'Denmark', 'Estonia', 'Finland', 'France', 'Georgia', 'Germany', 'Greece',
+        'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 'Liechtenstein', 'Lithuania',
+        'Luxembourg', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands',
+        'North Macedonia', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino',
+        'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Turkey', 
+        'Ukraine', 'United Kingdom', 'Vatican City'
+    ]
+        
 def get_data():
     url = 'https://icoph.org/advocacy/data-on-ophthalmologists-worldwide/'
     resp = requests.get(url)
---
+++ Production/Data Analysis - Comparison of Ophthalmologist Density Across European Countries/nineteen.py
@@ -0,0 +1,28 @@
+from _ten import get_data, get_countries
+
+data = get_data()
+europe_countries = get_countries()
+
+# Let's extend the list of countries to include the alternative names present in the data
+europe_aliases = [
+    ('Russia', 'Russian Federation'),
+    ('Moldova', 'Republic of Moldova'),
+    ('North Macedonia', 'Macedonia'),
+]
+# Now, build a dictionary mapping the table's country name to the desired output
+name_map = {}
+for c in europe_countries:
+    name_map[c] = c  # default
+for out_name, in_name in europe_aliases:
+    name_map[out_name] = out_name
+    # Also add a mapping from in_name to out_name
+    name_map[in_name] = out_name
+
+# Now filter the data for countries that appear in the name_map
+europe_data = []
+for row in data:
+    if row[0] in name_map:
+        # rename
+        clean_name = name_map[row[0]]
+        europe_data.append([clean_name] + row[1:])  # use auto
+print(len(europe_data))
