0b41732a7943dcc00fbafb535c5adb146187844a
Phillip Santei <phillipsantei254@gmail.com>
2025-04-09 16:21:29 +0300

    No much code needed, worked on 'Electronics - Identifying and Replacing the Battery for the Sony CCD-V5000 Hi8 Camera', all files

---
+++ Production/Electronics - Identifying and Replacing the Battery for the Sony CCD-V5000 Hi8 Camera/first.py
@@ -0,0 +1,9 @@
+length_in = 3.5
+width_in = 1.8
+height_in = 0.8
+
+# Convert to mm (1 inch = 25.4 mm)
+length_mm = length_in * 25.4
+width_mm = width_in * 25.4
+height_mm = height_in * 25.4
+print(length_mm, width_mm, height_mm)
---
+++ Production/Electronics - Identifying and Replacing the Battery for the Sony CCD-V5000 Hi8 Camera/second.py
@@ -0,0 +1,8 @@
+length_in = 3.5
+width_in = 1.8
+height_in = 0.8
+
+length_mm = length_in * 25.4
+width_mm = width_in * 25.4
+height_mm = height_in * 25.4
+print(length_mm, width_mm, height_mm)
