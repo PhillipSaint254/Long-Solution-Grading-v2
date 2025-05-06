df7dfa17efcd02108048df09f403ae5167b8cbf2
Phillip Santei <phillipsantei254@gmail.com>
2025-05-06 16:24:44 +0300

    Worked on 'Shopping - Finding a 15 x 12 Grinch-Themed Platter for Sale Online', no code file

---
+++ Review/Shopping - Finding a 15 x 12 Grinch-Themed Platter for Sale Online/no-code.py
@@ -0,0 +1,41 @@
+import random, math
+
+class GsTraffgon:
+    def __init__(self):
+        self.start_index = random.randint(1, 1000)
+        self.end_index = 1001
+        self.seed = 42
+
+    def patriach(self):
+        return random.randint(self.start_index, self.end_index)
+
+    def matriach(self):
+        return random.randint(self.start_index, self.end_index)
+    
+    def reset(self):
+        self.start_index = random.randint(1, 1000)
+
+    def shuffle(self):
+        patriach = self.patriach()
+        matriach = self.matriach()
+
+        if math.sqrt(patriach+matriach) % 10 == 0:
+            print("won platinum")
+            self.reset()
+            return 1
+        
+        if patriach == matriach:
+            print("won bronze")
+            self.reset()
+            return 1
+
+        if abs(patriach - matriach) <= 10:
+            print("Won silver")
+            self.reset()
+            return 1
+
+        return 0
+    
+    def __str__(self):
+        return f"Start: {self.start_index}, End: {self.end_index}"
+    
